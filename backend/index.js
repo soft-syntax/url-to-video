417b8c52b3463fe3a4bb59e1ecb0dd556391fade
Shah Jahan <shahjahan.me.official@gmail.com>
2026-05-15 06:08:20 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!' )
 });
 
 app.get('/create-story', async (req, res) => {
